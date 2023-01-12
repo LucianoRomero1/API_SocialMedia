b43e04ef1fc4d0d73503ff86e5c067eafb34c74c
LucianoRomero1 <lucianooromero1@gmail.com>
2023-01-12 11:02:12 -0300

    follow model, follow and unfollow actions

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,9 +1,58 @@
-const testFollow = (req, res) => {
-  return res.status(200).send({
-    message: "Message sended from: controllers/follow.js",
+const Follow = require("../models/follow");
+const User = require("../models/user");
+
+const save = (req, res) => {
+  const params = req.body;
+
+  const identity = req.user;
+
+  let userToFollow = new Follow({
+    user: identity.id,
+    followed: params.followed,
+  });
+
+  userToFollow.save((error, followStored) => {
+    if (error || !followStored) {
+      return res.status(500).send({
+        status: "error",
+        message: "Could not follow user",
+      });
+    }
+
+    return res.status(200).send({
+      status: "success",
+      identity: req.user,
+      follow: followStored,
+    });
+  });
+};
+
+const unfollow = (req, res) => {
+  const userId = req.user.id;
+
+  const followedId = req.params.id;
+
+  Follow.find({
+    user: userId,
+    followed: followedId,
+  }).remove((error, followDeleted) => {
+    if (error || !followDeleted) {
+      return res.status(500).send({
+        status: "error",
+        message: "Error when unfollowing user",
+      });
+    }
+
+    return res.status(200).send({
+      status: "success",
+      message: "User unfollowed successfully",
+      identity: req.user,
+      followDeleted,
+    });
   });
 };
 
 module.exports = {
-  testFollow,
+  save,
+  unfollow,
 };
--- routes/follow.js
+++ routes/follow.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const FollowController = require("../controllers/follow");
+const check = require("../middlewares/auth");
 
-router.get("/test-follow", FollowController.testFollow);
+router.post("/save", check.auth, FollowController.save);
+router.delete("/unfollow/:id", check.auth, FollowController.unfollow);
+
 
 module.exports = router;
